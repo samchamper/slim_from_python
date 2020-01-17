b49aa81f39db5f043fcc0d2a5e56bf0931fbf6c8
samchamper <samchamper@gmail.com>
2020-01-16 20:31:58 -0500

    Add os detection to matplotlib script.

--- matplotlib_script.py
+++ matplotlib_script.py
@@ -8,15 +8,17 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import pandas as pd
 import numpy as np
+import os
 
 #We'll graph:
 # 'Drive homing rate' (col 0) on the X, 
 # 'Resistance formation rate' (col 1) on Y,
 # and outcome (final drive rate, (col 3)) on Z.
-data = pd.read_csv(r"SGE_data\output.csv")
+data = pd.read_csv("SGE_data{}output.csv".format('\\' if os.name == 'nt' else '/')) # Use the right slash for the OS we're using.
 homing_rate = data.iloc[:,0]
 res_rate = data.iloc[:,1]
 final_rate = data.iloc[:,3]
+final_pop = data.iloc[:,7]
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
