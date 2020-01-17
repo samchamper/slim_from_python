94b0f901c1699db738067cdf681f89450c5c022a
samchamper <samchamper@gmail.com>
2020-01-17 15:52:36 -0500

    Fix typo.

--- generate_cluster_params_file.py
+++ generate_cluster_params_file.py
@@ -3,7 +3,7 @@ homing = 0.5
 res = 0.0
 print_header = True
 for homing_steps in range(51):
-    for resistence_steps in range(51):
+    for resistance_steps in range(51):
         print(f"python3 minimal_slim_driver.py -homing {homing:.2f} -res {res:.3f}{' -header' if print_header else ''}")
         print_header = False
         res += 0.001
--- generate_large_local_array_run.py
+++ generate_large_local_array_run.py
@@ -4,7 +4,7 @@ res = 0.0
 print_header = True
 run_number = 1
 for homing_steps in range(11):
-    for resistence_steps in range(11):
+    for resistance_steps in range(11):
         print(f"python3 minimal_slim_driver.py -homing {homing:.2f} -res {res:.3f}{' -header' if print_header else ''} > py_data/{run_number}.part &")
         print_header = False
         run_number += 1
